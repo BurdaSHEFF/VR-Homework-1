9c4af11985901b883a6c9880c9bc1a1684857cbe
Alexandr Klushin <43380333+BurdaSHEFF@users.noreply.github.com>
2021-09-22 16:18:57 +0500

    Create

---
+++ Hello World/Assets/Scripts/Jump.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    private bool isGrounded;
+    public GameObject obj;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void OnCollisionEnter()
+    {
+        isGrounded = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space) && isGrounded)
+        {
+            isGrounded = false;
+            GetComponent<Rigidbody>().AddForce(new Vector3(0, 200, 0));
+        }
+    }
+}
